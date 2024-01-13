1b11e724f4f312b62c091defe47e442e1c4804f5
“Reese <reesegreen2014@me.com>
2024-01-13 12:56:21 -0700

    Initial commit

--- functions.js
+++ functions.js
@@ -4,17 +4,41 @@
 // If a function takes parameters, call the function at least twice, passing different arguments each time.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
-
+function giveAGreeting() {
+    return "Thanks for visiting!";
+}
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
-
+function customGreeting(name) {
+    return "Thanks for visiting, Reese!";
+}
+console.log(customGreeting("Reese"));
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+
+var first = "Reese";
+var middle = "Jared";
+var last = "Green";
+
+function greetPerson(first, middle, last) {
+    return `Thanks for visiting ${first} ${middle} ${last}!`;
+}
+
+console.log(greetPerson(first, middle, last))
+
+//I would've got this sooner if I remembered to use backtics... SMH! 
 
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
+function square(number) {
+    return number * number
+}
+let chosenNumber = 5;
+let answer = square(chosenNumber); 
+console.log(`The square of ${chosenNumber} is ${answer}.`);
 
+//Had to Google for this one... I was stuck! 
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
